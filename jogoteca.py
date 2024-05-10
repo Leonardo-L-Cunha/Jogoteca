9d6d594776f0cb07746e63f743d51d35aac5035f
Leonardo <leonavidareal@gmail.com>
2024-05-10 18:01:06 -0300

    uptade

--- jogoteca.py
+++ jogoteca.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html', titulo='Jogos')
+    lista = ['Halo', 'Mass Effect', 'Metal gear']
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
 app.run(host='localhost', port=8080)

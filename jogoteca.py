a520c11b31cae66d0d68bf1546e2d96b95351b32
Leonardo <leonavidareal@gmail.com>
2024-05-16 15:59:42 -0300

    feat: redirect novo

--- jogoteca.py
+++ jogoteca.py
@@ -24,7 +24,7 @@ def index():
 @app.route('/novo')
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect('/login')
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo jogo')
 
 
@@ -42,14 +42,16 @@ def criar():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 @app.route('/autenticar', methods=['POST'])
 def autencticar():
     if '1234' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(f'{session['usuario_logado'] } logado com sucesso!')
-        return redirect('/')
+        proxima_pagina = request.form['proxima']
+        return redirect('/{}'.format(proxima_pagina))
     else:
         flash('Usuario ou senha invalidos')
         return redirect('/login')

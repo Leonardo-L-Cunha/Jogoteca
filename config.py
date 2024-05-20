82ba96a61f3a0616af7cf2e1d94387bcaa9239a5
Leonardo <leonavidareal@gmail.com>
2024-05-20 13:30:39 -0300

    feat: reestruturando codigo

---
+++ config.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'pokemon123'
+
+SQLALCHEMY_DATABASE_URI = '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+    SGBD = 'mysql+mysqlconnector',
+    usuario = 'root',
+    senha = '1988',
+    servidor = 'localhost',
+    database = 'jogoteca'
+)

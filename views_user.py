f79ba74969ce1be1a3f95790256702698999b8d9
Leonardo <leonavidareal@gmail.com>
2024-05-24 12:50:08 -0300

    feat: criptografrando senha

--- views_user.py
+++ views_user.py
@@ -1,8 +1,8 @@
 from app import app 
 from flask import request, redirect, session, flash, url_for, render_template
-from helpers import FormularioUsuario
 from models import Usuarios
-
+from helpers import FormularioUsuario
+from flask_bcrypt import check_password_hash
 
 @app.route('/login')
 def login():
@@ -13,17 +13,16 @@ def login():
 @app.route('/autenticar', methods=['POST'])
 def autenticar():
     form = FormularioUsuario(request.form)
-
-
     usuario = Usuarios.query.filter_by(nickname=form.nickname.data).first()
-    if usuario:
-        if form.senha.data == usuario.senha:
-            session['usuario_logado'] = usuario.nickname
-            flash(f'{usuario.nickname} logado com sucesso!')
-            if request.form['proxima'] != 'novo':
-                return redirect(url_for('index'))
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
+    senha = check_password_hash(usuario.senha, form.senha.data)
+
+    if usuario and senha:
+       session['usuario_logado'] = usuario.nickname
+       flash(f'{usuario.nickname} logado com sucesso!')
+       if request.form['proxima'] != 'novo':
+            return redirect(url_for('index'))
+       proxima_pagina = request.form['proxima']
+       return redirect(proxima_pagina)
     else:
         flash('Usuario ou senha invalidos')
         return redirect(url_for('login'))
